3b0bc9eb91911134dc96c5b38cabea150b75b7ca
alexandrenavarro <alexandre.j.navarro@gmail.com>
2013-10-18 00:28:18 +0200

    Manage checkbox

--- src/main/java/com/github/springmvc/Person.java
+++ src/main/java/com/github/springmvc/Person.java
@@ -1,9 +1,14 @@
 package com.github.springmvc;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Person {
     
     private String firstName;
     private String lastName;
+    private List<String> test;
+    //private String[] test;
     
     public String getFirstName() {
         return this.firstName;
@@ -17,10 +22,31 @@ public class Person {
     public void setLastName(String aLastName) {
         this.lastName = aLastName;
     }
+//    public String[] getTest() {
+//        return this.test;
+//    }
+//    public void setTest(String[] aTest) {
+//        this.test = aTest;
+//    }
+//    @Override
+//    public String toString() {
+//        return "Person [firstName=" + this.firstName + ", lastName=" + this.lastName + ", test=" + Arrays.toString(this.test) + "]";
+//    }
+    public List<String> getTest() {
+        return this.test;
+    }
+    public void setTest(List<String> aTest) {
+        this.test = aTest;
+    }
     @Override
     public String toString() {
-        return "Person [firstName=" + this.firstName + ", lastName=" + this.lastName + "]";
+        return "Person [firstName=" + this.firstName + ", lastName=" + this.lastName + ", test=" + this.test + "]";
     }
+    
+    
+    
+    
+    
     
     
     
